228709e2c2e37ac2df519ec385851f2e2a1bb0b0
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-10 19:21:19 +0200

    removed double border from header, deleted nested link

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -4,21 +4,20 @@ import { ROUTES } from '../../consts';
 import styles from './Dashboard.module.css';
 
 
+/* misschien is header eigenlijk een logischere naam voor deze component? */
 const Dashboard = () => {
   return (
     <>
       <div className={styles.container}>
-
         {/* Header dashboard */}
         <div className={styles.dashboard__header}>
           <img className={styles.img} src='./assets/img/fyfr.svg' alt="group img" width="37" height="39"/>
           <div className="dashboard__views">
-            <Link to={`${ROUTES.ancestors}`}>
-              <a className={styles.tab}>Family Tree</a>
+            <Link to={`${ROUTES.ancestors}`} className={styles.tab}>
+              Family Tree
             </Link>
-
-            <Link to={`${ROUTES.map}`}>
-              <a className={styles.tab}>Roots DNA</a>
+            <Link to={`${ROUTES.map}`}className={styles.tab}>
+              Roots DNA
             </Link>
           </div>
           <img className={styles.img} src='./assets/img/hamburger.svg' alt="group img" width="24" height="12" />

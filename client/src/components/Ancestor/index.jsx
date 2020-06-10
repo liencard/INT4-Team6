c436f429387fc9c72ddebea10a9840b7e9b4b3f3
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-10 23:11:37 +0200

    get right ancestor on click in WebGL environment

--- client/src/components/Ancestor/index.jsx
+++ client/src/components/Ancestor/index.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
-import { useSpring, a } from 'react-spring/three';
+import React, { useState, useCallback } from 'react';
 import * as THREE from 'three';
-import { HTML, Text } from 'drei';
+import { useSpring, a } from 'react-spring/three';
+import { useFrame, useThree } from 'react-three-fiber';
+// import { computeBoundsTree, disposeBoundsTree, acceleratedRaycast } from 'three-mesh-bvh'; 
+// bvh deleten uit package.json indien niet gebruiken voor id op te halen raycaster
+
+import { Text } from 'drei';
 import styles from '../Ancestor/Ancestor.module.css';
 // import { Link } from 'react-router-dom';
 // import { ROUTES } from '../../consts';
-import { useThree } from 'react-three-fiber';
+
 
 const Ancestor = ({ancestor}) => {
   // foto inladen
@@ -26,35 +30,78 @@ const Ancestor = ({ancestor}) => {
   let posY = position[1];
   let posZ = position[2];
 
-  const {raycaster} = useThree();
+
+
+  const mesh = React.useRef();
+
+  // enkel eerste item onder muis = intersects[0] ?
+  // const { raycaster, scene, intersect } = useThree();
+  // console.log(intersect());
+
+  // let intersectedObjects = [];
+  // intersectedObjects = raycaster.intersectObjects(scene.children);
+
+  // alle uuid
+  // if (mesh.current) {
+  //     console.log(mesh.current.uuid);
+  // }
+
+  // console.log(intersect)
+  // useFrame(() => {
+  //   intersect = raycaster.intersectObject(scene.children);
+  //   if (intersect) {
+  //     console.log(intersect);
+  //   }
+  // });
+
+  // three-mesh-bvh probeersel
+  // raycaster.firstHitOnly = true;
+  // let intersect = raycaster.intersectObjects(scene.children);
+
+  // window.addEventListener('mousemove', onMouseMove, false);
+  // function onMouseMove( event ) {
+  //   // console.log("test")
+  // }
+
+  const toggleHover = useCallback((e, value) => {
+      e.stopPropagation(); /// !!!!
+      setHover(value)
+  });
+
+  const handleClickAncestor = (e) => {
+    e.stopPropagation(); /// !!!!
+    console.log(e.eventObject.uuid); // findId in store op basis van deze id? uuid is wel random TO CHECK, uniek id meegeven als prop?
+  };
 
   return (
     <>
-        <Text
-          position={[posX - 0.6, posY + 0.7, posZ + 0.2]}
-          fontSize={0.15}
-          color={'white'}
-          font={'./assets/fonts/DMSerifDisplay-Regular.ttf'}
-        >
-          {ancestor.name}
-        </Text>
-        <Text
-          position={[posX - 0.6, posY + 0.5, posZ + 0.2]}
-          fontSize={0.1}
-          color={'white'}
-        >
-          1850 - 1940
-        </Text>
+      <Text
+        position={[posX - 0.6, posY + 0.7, posZ + 0.2]}
+        fontSize={0.15}
+        color={'white'}
+        font={'./assets/fonts/DMSerifDisplay-Regular.ttf'}
+      >
+        {ancestor.name}
+      </Text>
+      <Text
+        position={[posX - 0.6, posY + 0.5, posZ + 0.2]}
+        fontSize={0.1}
+        color={'white'}
+      >
+        1850 - 1940
+      </Text>
 
-        <a.mesh
-          onPointerOver={(e) => setHover(true)}
-          onPointerOut={(e) => setHover(false)}
-          scale={states.scale}
-          position={[posX, posY, posZ]}
-        >
-          <planeGeometry attach="geometry" args={[1, 1]} />
-          <a.meshBasicMaterial attach="material" map={img} transparent={true} />
-        </a.mesh>
+      <a.mesh
+        ref={mesh} // mag weg later
+        onPointerOver={(e) => toggleHover(e, true)}
+        onPointerOut={(e) => toggleHover(e, false)}
+        onClick={(e) => handleClickAncestor(e)}
+        scale={states.scale}
+        position={[posX, posY, posZ]}
+      >
+        <planeGeometry attach="geometry" args={[1, 1]} />
+        <a.meshBasicMaterial attach="material" map={img} transparent={true} />
+      </a.mesh>
     </>
   );
 };

1559ba8b4608bec05814b1cb125c1b37ddfcfc3e
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-11 14:39:37 +0200

    started stylesheet sidebar, content preview sidebar for ancestor

--- client/src/components/Preview/index.jsx
+++ client/src/components/Preview/index.jsx
@@ -5,10 +5,29 @@ import styles from './Preview.module.css';
 const Preview = ({ancestor}) => {
 
   return useObserver(() => (
-      <>
-        <h2>{ancestor.name}</h2>
-        <p>Geboorte - Sterfte</p>
-        <p>Intro</p>
+    <>
+      <div className={styles.preview__wrapper}>
+        <img
+          className={styles.image}
+          src="./assets/img/ancestor_george.png"
+          alt=""
+          height="140"
+          width="140"
+        />
+
+        <div className={styles.header}>
+          <h2 className={styles.name}>{ancestor.name}</h2>
+          <p className={styles.dates}>
+            {ancestor.birthdate} - {ancestor.deathdate}
+          </p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <button>Button</button>
     </>
   ));
 };
--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -17,7 +17,7 @@ const Sidebar = ({type, content, toggle, setToggle}) => {
 
   return useObserver(() => (
     <div className={`${styles.sidebar} ${toggle ? styles.sidebarOpened : styles.sidebarClosed}`}>
-        <button className={styles.closeButton} onClick={() => setToggle(false)}>close</button>
+        <button className={styles.close} onClick={() => setToggle(false)}>close</button>
         <Content />
     </div>
   ));

8c6ab3ddb3d102f104b4c6ee0dd6a143d88cce9e
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-11 19:12:36 +0200

    cleaning up files

--- client/src/components/Ancestor/index.jsx
+++ client/src/components/Ancestor/index.jsx
@@ -1,16 +1,8 @@
 import React, { useState, useCallback } from 'react';
 import * as THREE from 'three';
 import { useSpring, a } from 'react-spring/three';
-import { useFrame, useThree } from 'react-three-fiber';
-// import { computeBoundsTree, disposeBoundsTree, acceleratedRaycast } from 'three-mesh-bvh'; 
-// bvh deleten uit package.json indien niet gebruiken voor id op te halen raycaster
 
 import { Text, HTML } from 'drei';
-import styles from '../Ancestor/Ancestor.module.css';
-// import { Link } from 'react-router-dom';
-// import { ROUTES } from '../../consts';
-
-
 const Ancestor = ({ ancestor, ancestorStore }) => {
   // foto inladen
   const img = new THREE.TextureLoader().load(
@@ -34,39 +26,8 @@ const Ancestor = ({ ancestor, ancestorStore }) => {
 
   const dates = `${ancestor.birthdate} - ${ancestor.deathdate}`;
 
-  const mesh = React.useRef();
-
-  // enkel eerste item onder muis = intersects[0] ?
-  // const { raycaster, scene, intersect } = useThree();
-  // console.log(intersect());
-
-  // let intersectedObjects = [];
-  // intersectedObjects = raycaster.intersectObjects(scene.children);
-
-  // alle uuid
-  // if (mesh.current) {
-  //     console.log(mesh.current.uuid);
-  // }
-
-  // console.log(intersect)
-  // useFrame(() => {
-  //   intersect = raycaster.intersectObject(scene.children);
-  //   if (intersect) {
-  //     console.log(intersect);
-  //   }
-  // });
-
-  // three-mesh-bvh probeersel
-  // raycaster.firstHitOnly = true;
-  // let intersect = raycaster.intersectObjects(scene.children);
-
-  // window.addEventListener('mousemove', onMouseMove, false);
-  // function onMouseMove( event ) {
-  //   // console.log("test")
-  // }
-
   const toggleHover = useCallback((e, value) => {
-    e.stopPropagation(); /// !!!!
+    e.stopPropagation();
     setHover(value); // flicker bug
   }); 
 
@@ -89,11 +50,9 @@ const Ancestor = ({ ancestor, ancestorStore }) => {
       </Text>
 
       <a.mesh
-        ref={mesh} // mag weg later
         ancestorId={ancestor.id}
         onPointerOver={(e) => toggleHover(e, true)}
         onPointerOut={(e) => toggleHover(e, false)}
-        // onClick={(e) => handleClickAncestor(e)}
         scale={states.scale}
         position={[posX, posY, posZ]}
       >

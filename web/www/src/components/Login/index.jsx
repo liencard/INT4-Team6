de8240ba9d4be682a54188f51566d299a72bd55c
liencard <lien.cardoen@student.howest.be>
2020-06-13 21:24:01 +0200

    finished login

--- web/www/src/components/Login/index.jsx
+++ web/www/src/components/Login/index.jsx
@@ -6,6 +6,7 @@ import Button from '../Button/index.jsx';
 const Login = () => {
 
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleSubmitLogin = async (e) => {
       e.preventDefault();
@@ -16,53 +17,78 @@ const Login = () => {
       className={styles.login__wrapper}
       style={{ backgroundImage: `url(assets/img/lines_bg.png)` }}
     >
+      <div className={styles.header}>
+        <div className={styles.header__wrapper}>
+          <img
+            className={styles.img}
+            src="./assets/img/FYFR.svg"
+            alt="group img"
+            width="37"
+            height="39"
+          />
+          <p className={styles.header__text}>Login</p>
+          <img
+            className={styles.img}
+            src="./assets/img/hamburger.svg"
+            alt="group img"
+            width="24"
+            height="12"
+          />
+        </div>
+      </div>
 
-        <div className={styles.header}>
-            <div className={styles.header__wrapper}>
-                <img className={styles.img} src='./assets/img/FYFR.svg' alt="group img" width="37" height="39"/>
-                <p className={styles.header__text}>Login</p>
-                <img className={styles.img} src='./assets/img/hamburger.svg' alt="group img" width="24" height="12" />
+      <div
+        className={styles.login__img}
+        style={{ backgroundImage: `url(assets/img/login.png)` }}
+      ></div>
+
+      <div className={styles.content__wrapper}>
+        <h1 className={styles.title}>Welcome back</h1>
+        <p className={styles.subtitle}>Sign in to your account</p>
+
+        <form className={styles.form} onSubmit={handleSubmitLogin}>
+          <div className={styles.form__wrapper}>
+            <label className={styles.form__label}>Email</label>
+            <input
+              className={styles.form__input}
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+              required
+            />
+            <label className={styles.form__label}>Password</label>
+            <input
+              className={styles.form__input}
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+              required
+            />
+
+            <div className={styles.form__extra}>
+              <a href="#">forgot password?</a>
+              <label>
+                <input type="checkbox" name="remember" />
+                Remember me
+              </label>
             </div>
-        </div>
+          </div>
+          <Button text={'login'} to={'onboarding-one'} />
 
-        <div
-          className={styles.login__img}
-          style={{ backgroundImage: `url(assets/img/login.png)` }}
-        ></div>
+          <div className={styles.divider}>
+            <div className={styles.line}></div>
+            <p>or</p>
+            <div className={styles.line}></div>
+          </div>
+        </form>
 
-        <div className={styles.form__wrapper}>
-            <h1 className={styles.title}>Welcome back</h1>
-            <p className={styles.subtitle}>Sign in to your account</p>
-
-            <form className={styles.form} onSubmit={handleSubmitLogin}>
-                <label className={styles.form__label}>Email</label>
-                <input
-                  className={styles.form__input}
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={e => setEmail(e.currentTarget.value)}
-                  required
-                />
-                <label className={styles.form__label}>Email</label>
-                <input
-                  className={styles.form__input}
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={e => setEmail(e.currentTarget.value)}
-                  required
-                />
-                <Button text={'login'} to={'onboarding-one'} className={styles.button}/>
-            </form>
-        </div>
-
-
-
+        <p className={styles.subtext}>
+          Don't have an account? <a href="#">Sign up</a>
+        </p>
+      </div>
     </div>
-
-
-
   ));
 };
 

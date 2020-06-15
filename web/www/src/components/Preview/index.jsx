1902e5b39620468a9e5f1f3a5bf7a5aabd6e7dad
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-15 15:30:40 +0200

    thumbnail on preview ancestor

--- web/www/src/components/Preview/index.jsx
+++ web/www/src/components/Preview/index.jsx
@@ -4,13 +4,15 @@ import styles from './Preview.module.css';
 import Button from '../Button/index.jsx';
 
 const Preview = ({ancestor}) => {
+  const imgName = ancestor.name.split(' ').join('');
+  console.log(ancestor.name);
 
   return useObserver(() => (
     <>
       <div className={styles.preview__wrapper}>
         <img
           className={styles.image}
-          src="./assets/img/ancestor_george.png"
+          src={`./assets/img/ancestors/thumbnail/${imgName}.jpg`}
           alt=""
           height="140"
           width="140"
@@ -28,17 +30,35 @@ const Preview = ({ancestor}) => {
         </p>
 
         <div className={styles.info}>
-          <img className={styles.icon} src='./assets/img/icon_location.svg' alt="icon of location pin" width="21" height="25"/>
+          <img
+            className={styles.icon}
+            src="./assets/img/icon_location.svg"
+            alt="icon of location pin"
+            width="21"
+            height="25"
+          />
           <div className={styles.info__wrapper}>
             <p className={styles.info__title}>Georgia</p>
             <p className={styles.info__subtitle}>United States of America</p>
           </div>
-          <img className={styles.icon} src='./assets/img/icon_work.svg' alt="icon of work bag" width="24" height="22"/>
+          <img
+            className={styles.icon}
+            src="./assets/img/icon_work.svg"
+            alt="icon of work bag"
+            width="24"
+            height="22"
+          />
           <div className={styles.info__wrapper}>
             <p className={styles.info__title}>Farmer</p>
             <p className={styles.info__subtitle}>Working at cotton fields</p>
           </div>
-          <img className={styles.icon} src='./assets/img/icon_period.svg' alt="icon of travel back time icon" width="22" height="21"/>
+          <img
+            className={styles.icon}
+            src="./assets/img/icon_period.svg"
+            alt="icon of travel back time icon"
+            width="22"
+            height="21"
+          />
           <div className={styles.info__wrapper}>
             <p className={styles.info__title}>The american revolution</p>
             <p className={styles.info__subtitle}>1776 - 1789</p>

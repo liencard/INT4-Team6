8a53e2978b1e479ca93613b0115e077dcb1b33b1
liencard <lien.cardoen@student.howest.be>
2020-06-16 21:23:11 +0200

    added states to menu

--- web/www/src/components/Menu/index.jsx
+++ web/www/src/components/Menu/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useObserver } from 'mobx-react-lite';
 import { NavLink } from 'react-router-dom';
 import { ROUTES } from '../../consts';
@@ -7,14 +7,47 @@ import styles from './Menu.module.css';
 import Button from '../Button/index.jsx';
 
 const Menu = () => {
-  const { uiStore, userStore } = useStore();
 
-  console.log(userStore.users);
-  console.log(uiStore.currentUser);
+  const STATE_LOADING = 'loading';
+  const STATE_LOADED = 'loaded';
+  const STATE_NOT_FOUND = "notFound";
 
-  return useObserver(() => (
+  const { uiStore } = useStore();
+
+  const [currentUser, setCurrentUser] = useState(uiStore.currentUser);
+  const [state, setState] = useState(STATE_LOADING);
+
+  useEffect(() => {
+
+    const loadCurrentUser = async () => {
+      try {
+        const currentUser = await uiStore.currentUser;
+        if (!currentUser) {
+          setState(STATE_LOADING);
+          return;
+        }
+        setCurrentUser(currentUser);
+        setState(STATE_LOADED);
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setState(STATE_NOT_FOUND);
+        }
+      }
+    }
+
+    loadCurrentUser();
+    
+  }, [uiStore, setCurrentUser])
+
+  return useObserver(() => {
+    if (state === STATE_LOADING) {
+      return 'loading user';
+    }
+    return (
     <>
       <div className={styles.menu__wrapper}>
+
+        {/* current user */}
         <img
           className={styles.image}
           src={uiStore.currentUser.avatar}
@@ -22,9 +55,9 @@ const Menu = () => {
           height="140"
           width="140"
         />
+        <h2 className={styles.name}>{currentUser.name}</h2>
 
-        <h2 className={styles.name}>{uiStore.currentUser.name}</h2>
-
+        {/* menu  */}
         <div className={styles.menu__nav}>
           <NavLink
             to={`${ROUTES.ancestors}`}
@@ -65,7 +98,8 @@ const Menu = () => {
       </div>
       <Button text={'logout'} to={'link'} />
     </>
-  ));
+    )
+  });
 };
 
 export default Menu;

c224cc05c995529af00b7426db45480324de491d
liencard <lien.cardoen@student.howest.be>
2020-06-16 12:36:41 +0200

    small fix

--- web/www/src/contexts/index.js
+++ web/www/src/contexts/index.js
@@ -8,7 +8,7 @@ const loadAllData = async () => {
     await store.ancestorStore.loadAllAncestors();
     await store.userStore.loadAllUsers();
     await store.bookmarkStore.loadAllBookmarks();
-    await store.uiStore.setCurrentUser(store.userStore.resolveUser("4e8baf11-bb77-3f6b-97d1-69b8e51c2ebe"));
+    store.uiStore.setCurrentUser(store.userStore.resolveUser("4e8baf11-bb77-3f6b-97d1-69b8e51c2ebe"));
 };
 
 loadAllData();

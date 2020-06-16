c9b2a0ee6e75d9a5d488decc1143b9baa6ccd380
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-16 20:19:54 +0200

    updated popup CSS for map

--- web/www/src/components/Map/index.jsx
+++ web/www/src/components/Map/index.jsx
@@ -1,13 +1,10 @@
 import React, { useRef, useEffect, useState, useLayoutEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import styles from './Map.module.css';
-// import './Map.module.css';
 import { useObserver } from 'mobx-react-lite';
-import ReactTooltip from 'react-tooltip';
 import { useStore } from '../../hooks/useStore';
 
 import Sidebar from '../Sidebar/index.jsx';
-//import Marker from '../Ancestor/index.jsx';
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiY2FyZG9lbmxpZW4iLCJhIjoiY2tiODI3Znl4MDAyazJ4cXJ6cWNvdWswcSJ9.aVAGrbiyl5I5yb5KROaD7A';
@@ -20,7 +17,6 @@ const Map = () => {
   const ancestors =  ancestorStore.ancestors;
   const [preview, setPreview] = useState(false);
   const [ancestor, setAncestor] = useState(null);
-  //const [loading, setLoading] = useState('loading')
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -32,7 +28,6 @@ const Map = () => {
     });
 
     map.dragRotate.disable();
-
     let hoveredRegionId = null;
 
     // LOAD
@@ -132,21 +127,14 @@ const Map = () => {
     });
 
     map.on('zoomend', function () {
-      // if (map.getZoom() < 4) {
-      //   map.removeFeatureState({
-      //     source: 'regions',
-      //   });
-      // }
 
       ancestors.forEach((ancestor) => {
-        // mapboxgl-marker
         if (map.getZoom() > 2) {
           createMarker(ancestor, map);
         } else if (map.getZoom() < 2) {
           removeMarkers();
         }
       });
-
     });
 
     map.on('mouseenter', 'regions-layer', function (e) {
@@ -163,8 +151,6 @@ const Map = () => {
 
     return () => map.remove();
   }, []);
-  // ON LOAD
-
 
   const removeMarkers = () => {
     const $markers = document.querySelectorAll('.mapboxgl-marker');
@@ -172,24 +158,21 @@ const Map = () => {
   }
 
   const createMarker = (ancestor, map) => { 
-    // CSS neemt niet?
     const imgName = ancestor.name.split(' ').join('');
-    let content = `<img src="./assets/img/ancestors/thumbnail/${imgName}.jpg" class="popupImage" style="border-radius: 50%" height="60px" width="60px" />
-      <p className=${styles.test}>${ancestor.name}</p>
+    let content = `<img src="./assets/img/ancestors/thumbnail/${imgName}.jpg" 
+     height="60px" width="60px" />
+      <p>${ancestor.name}</p>
       <span>${ancestor.birthdate} - ${ancestor.deathdate}</span>`;
-    let popup = new mapboxgl.Popup({ offset: 25 }).setHTML(content);
+    let popup = new mapboxgl.Popup({
+      offset: 25,
+      className: styles.popup,
+      closeButton: false
+    }).setHTML(content);
 
-    // CREATE AND STYLE MARKER
+    // CREATE MARKER
     const el = document.createElement('div');
-    // el.classList.add(styles.marker);
-    // el.style.borderRadius = '50%';
-
-    // TEST VOOR TOOLTIP
-    el.innerHTML += `
-        <svg data-tip="test" height="10" width="10">
-          <circle cx="5" cy="5" r="5" fill="pink" />
-        </svg> 
-      `;
+    el.classList.add(styles.marker);
+    el.style.borderRadius = '50%';
 
     // FIX COORDINATES
     let mapCoordinates = [];
@@ -207,18 +190,6 @@ const Map = () => {
 
     const handleHoverMarker = (e) => {
       marker.togglePopup();
-      // refs
-      // let popup = document.getElementsByClassName('mapboxgl-popup')[0];
-      // if (popup) {
-      //   let popupContent = popup.getElementsByClassName(
-      //     'mapboxgl-popup-content'
-      //   )[0];
-      //   let popupTip = popup.getElementsByClassName(
-      //     'mapboxgl-popup-tip'
-      //   )[0];
-      //   popupContent.classList.add(styles.popupContent);
-      //   popupTip.classList.add(styles.popupTip);
-      // }
     };
 
     const handleClickAncestor = (e) => {
@@ -230,9 +201,7 @@ const Map = () => {
     markerDiv.addEventListener('mouseenter', handleHoverMarker);
     markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
     markerDiv.addEventListener('click', handleClickAncestor);
-
-
-} // END USER EFFECT?
+}
 
 
   return useObserver(() => (
@@ -242,17 +211,6 @@ const Map = () => {
         content={ancestor}
         toggle={preview}
         setToggle={setPreview}
-      />
-
-      {/* <div className={styles.test} >
-        <svg data-tip='test hellloooo werk :(' height="100" width="100">
-          <circle cx="50" cy="50" r="50" fill="pink" />
-        </svg> 
-      </div> */}
-
-      <ReactTooltip
-        //effect="solid"
-        //place="top"
       />
 
       <div className={styles.mapContainer} ref={mapContainerRef} />

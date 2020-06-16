c9de0526d0a8ec2bc06c033805358284358843e9
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-16 12:37:13 +0200

    Content detail page

--- web/www/src/components/Detail/index.jsx
+++ web/www/src/components/Detail/index.jsx
@@ -77,40 +77,63 @@ const Detail = () => {
       </article>
 
       <article className={`${styles.container} ${styles.living}`}>
-        <h2 className={styles.title}>Family Evans</h2>
-        <p className={styles.text}>
-          The family Evans lived in a very comfortable house. For the first
-          time, furniture was made in mass-production which meant that it was
-          cheaper. However this was in cost of the design. Due to this, middle
-          class families had overcrowded homes with lots of furtniture,
-          ornaments and knick-knacks.
-        </p>
-        <p className={styles.text}>
-          They had a house with two rooms upstairs and two downstairs. The two
-          rooms downstairs were used kitching and living room while thet wo
-          upstairs were bedrooms.
-        </p>
+        <div className={styles.living__text}>
+          <h2 className={styles.title}>Family Evans</h2>
+          <p className={styles.text}>
+            The family Evans lived in a very comfortable house. For the first
+            time, furniture was made in mass-production which meant that it was
+            cheaper. However this was in cost of the design. Due to this, middle
+            class families had overcrowded homes with lots of furtniture,
+            ornaments and knick-knacks.
+          </p>
+          <p className={styles.text}>
+            They had a house with two rooms upstairs and two downstairs. The two
+            rooms downstairs were used kitching and living room while thet wo
+            upstairs were bedrooms.
+          </p>
+        </div>
+        <div className={styles.living__img}>
+          <img
+            className={styles.living__imgFamily}
+            src="./assets/img/detail/MargeretEvans_family.png"
+            alt="Family"
+            width="260px"
+          />
+          <img
+            src="./assets/img/detail/MargeretEvans_house.png"
+            alt="House"
+            width="560px"
+          />
+        </div>
       </article>
 
       <article className={`${styles.container} ${styles.work}`}>
         <h2 className={styles.hidden}>Work</h2>
-        <section>
-          <h3 className={styles.title}>Sewer</h3>
-          <p className={styles.text}>
-            Back in the 1800’s a lot of women used to be charwomen, laundresses
-            or servant. While men and childeren usually worked in the factories
-            and did long shifts every day.
-          </p>
-        </section>
-        <section>
-          <h3 className={styles.title}>Clothes</h3>
-          <p className={styles.text}>
-            In the 19th century, only the rich could afford nice looking
-            clothing. Shoes, for example, were a luxery and not a lot of people
-            owned them. Being a sewer back in the day was a good job. These
-            types of clothing were called corsets and knickers.
-          </p>
-        </section>
+        <img
+          className={styles.work__img}
+          src="./assets/img/detail/MargeretEvans_work.png"
+          alt="Work"
+          width="800px"
+        />
+        <div className={styles.work__text}>
+          <section>
+            <h3 className={styles.title}>Sewer</h3>
+            <p className={styles.text}>
+              Back in the 1800’s a lot of women used to be charwomen,
+              laundresses or servant. While men and childeren usually worked in
+              the factories and did long shifts every day.
+            </p>
+          </section>
+          <section>
+            <h3 className={styles.title}>Clothes</h3>
+            <p className={styles.text}>
+              In the 19th century, only the rich could afford nice looking
+              clothing. Shoes, for example, were a luxery and not a lot of
+              people owned them. Being a sewer back in the day was a good job.
+              These types of clothing were called corsets and knickers.
+            </p>
+          </section>
+        </div>
       </article>
 
       <article className={styles.container}>
@@ -119,13 +142,21 @@ const Detail = () => {
           <span className={styles.timeframe__dates}>1815 - 1914</span>
         </div>
         <section className={styles.death__start}>
-          <h3 className={styles.subtitle}>The start</h3>
-          <p className={styles.text}>
-            During the 19th century life changed into the Industrial Revolution.
-            Margaret was born into this revolution as a sewer. At the start of
-            the century it caused many problems but later on the lifes of the
-            people became more comfortable for the ordinary.
-          </p>
+          <img
+            className={styles.death__img}
+            src="./assets/img/detail/MargeretEvans_death.png"
+            alt="Death"
+            width="540px"
+          />
+          <div className={styles.death__text}>
+            <h3 className={styles.subtitle}>The start</h3>
+            <p className={styles.text}>
+              During the 19th century life changed into the Industrial
+              Revolution. Margaret was born into this revolution as a sewer. At
+              the start of the century it caused many problems but later on the
+              lifes of the people became more comfortable for the ordinary.
+            </p>
+          </div>
         </section>
       </article>
     </>

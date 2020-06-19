c977a80ef9de29f2b64158bd54ae75333ad20e03
liencard <lien.cardoen@student.howest.be>
2020-06-20 00:59:39 +0200

    changes in ancestorDAO

--- web/www/dao/AncestorDAO.php
+++ web/www/dao/AncestorDAO.php
@@ -10,15 +10,24 @@ class AncestorDAO extends DAO {
   }
 
   // public function selectAll() {
-  //   $sql = "SELECT `int4_ancestors`.`id`, `int4_ancestors`.`user_id`, `int4_ancestors`.`name`, `int4_ancestors`.`coordinates`, `int4_ancestors`.`birthdate`, `int4_ancestors`.`deathdate`,
-  //   `int4_ancestors`.`intro`, `int4_ancestors`.`place`
-  //     FROM `int4_ancestors`
-  //     LEFT JOIN `int4_ancestors_relatives` 
-  //     ON `int4_ancestors`.`id` = `int4_ancestors_relatives`.`ancestor_id`";
-  //   $stmt = $this->pdo->prepare($sql);
-  //   $stmt->execute();
-  //   return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  //    $sql = "SELECT * FROM `int4_ancestors`
+  //      LEFT JOIN `int4_ancestors_relatives` 
+  //      ON `int4_ancestors`.`id` = `int4_ancestors_relatives`.`ancestor_id`";
+  //    $stmt = $this->pdo->prepare($sql);
+  //    $stmt->execute();
+  //    return $stmt->fetchAll(PDO::FETCH_ASSOC);
   // }
+
+  //  public function selectAll() {
+  //    $sql = "SELECT `int4_ancestors`.`id`, `int4_ancestors`.`user_id`, `int4_ancestors`.`name`, `int4_ancestors`.`coordinates`, `int4_ancestors`.`birthdate`, `int4_ancestors`.`deathdate`,
+  //    `int4_ancestors`.`intro`, `int4_ancestors`.`place`
+  //      FROM `int4_ancestors`
+  //      LEFT JOIN `int4_ancestors_relatives` 
+  //      ON `int4_ancestors`.`id` = `int4_ancestors_relatives`.`ancestor_id`";
+  //    $stmt = $this->pdo->prepare($sql);
+  //    $stmt->execute();
+  //    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  //  }
 
   public function selectById($id) {
     $sql = "SELECT * FROM `int4_ancestors` WHERE `id` = :id";

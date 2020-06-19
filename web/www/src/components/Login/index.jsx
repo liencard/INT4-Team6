32dd1f2682346c3163321f03075bc1e433dcd605
liencard <lien.cardoen@student.howest.be>
2020-06-19 11:53:33 +0200

    deactivated login function

--- web/www/src/components/Login/index.jsx
+++ web/www/src/components/Login/index.jsx
@@ -3,6 +3,7 @@ import styles from './Login.module.css';
 import { useHistory } from 'react-router-dom';
 import { useObserver } from 'mobx-react-lite';
 import { useStore } from '../../hooks/useStore';
+import { Link } from 'react-router-dom';
 import { ROUTES } from '../../consts';
 
 const Login = ({login}) => {
@@ -13,20 +14,17 @@ const Login = ({login}) => {
     const {uiStore, userStore} = useStore();
     console.log(uiStore.currentUser);
 
-    const handleSubmitLogin = async (e) => {
-      e.preventDefault();
-
-      //await userStore.loadAllUsers();
-      //uiStore.setCurrentUser(userStore.resolveUser('4e8baf11-bb77-3f6b-97d1-69b8e51c2ebe'));
-
-      if (
-        password === uiStore.currentUser.password &&
-        email === uiStore.currentUser.email
-      ) {
-        history.push(ROUTES.ancestors);
-      }
-
-    };  
+    // const handleSubmitLogin = async (e) => {
+    //   e.preventDefault();
+    //   //await userStore.loadAllUsers();
+    //   //uiStore.setCurrentUser(userStore.resolveUser('4e8baf11-bb77-3f6b-97d1-69b8e51c2ebe'));
+    //   if (
+    //     password === uiStore.currentUser.password &&
+    //     email === uiStore.currentUser.email
+    //   ) {
+    //     history.push(ROUTES.ancestors);
+    //   }
+    // };  
 
     return useObserver(() => (
       <div
@@ -37,7 +35,8 @@ const Login = ({login}) => {
         <h1 className={styles.title}>Welcome back</h1>
         <p className={styles.subtitle}>Sign in to your account</p>
 
-        <form className={styles.form} onSubmit={handleSubmitLogin}>
+        {/* <form className={styles.form} onSubmit={handleSubmitLogin}> */}
+        <form className={styles.form}>
           <div className={styles.form__wrapper}>
             <label className={styles.form__label}>Email</label>
             <input
@@ -46,7 +45,7 @@ const Login = ({login}) => {
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.currentTarget.value)}
-              required
+              //required
             />
             <label className={styles.form__label}>Password</label>
             <input
@@ -55,7 +54,7 @@ const Login = ({login}) => {
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.currentTarget.value)}
-              required
+              //required
             />
 
             <div className={styles.form__extra}>
@@ -69,7 +68,10 @@ const Login = ({login}) => {
               </label>
             </div>
           </div>
-          <button className={styles.button}>Login </button>
+          {/* <button className={styles.button} to={'/onboarding-one'}>Login </button> */}
+          <Link to={'/onboarding-one'} className={styles.button} style={{backgroundImage: `url(./assets/img/arrow.svg)`}}>
+            Login
+          </Link>
 
           <div className={styles.divider}>
             <div className={styles.line}></div>

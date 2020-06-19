8977e92745e0ecead83aba0dc5da8bf8dc49f8ab
liencard <lien.cardoen@student.howest.be>
2020-06-20 00:45:04 +0200

    tried sql linken relatives

--- web/www/dao/AncestorDAO.php
+++ web/www/dao/AncestorDAO.php
@@ -9,6 +9,17 @@ class AncestorDAO extends DAO {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
+  // public function selectAll() {
+  //   $sql = "SELECT `int4_ancestors`.`id`, `int4_ancestors`.`user_id`, `int4_ancestors`.`name`, `int4_ancestors`.`coordinates`, `int4_ancestors`.`birthdate`, `int4_ancestors`.`deathdate`,
+  //   `int4_ancestors`.`intro`, `int4_ancestors`.`place`
+  //     FROM `int4_ancestors`
+  //     LEFT JOIN `int4_ancestors_relatives` 
+  //     ON `int4_ancestors`.`id` = `int4_ancestors_relatives`.`ancestor_id`";
+  //   $stmt = $this->pdo->prepare($sql);
+  //   $stmt->execute();
+  //   return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  // }
+
   public function selectById($id) {
     $sql = "SELECT * FROM `int4_ancestors` WHERE `id` = :id";
     $stmt = $this->pdo->prepare($sql);
@@ -16,15 +27,5 @@ class AncestorDAO extends DAO {
     $stmt->execute();
     return $stmt->fetch(PDO::FETCH_ASSOC);
   }
-
-  // public function getValidationErrors($data) {
-  //   $errors = array();
-  //   if(!isset($data['user_id'])) {
-  //     $errors['user_id'] = "Please fill in a user id";
-  //   }
-  //   if(!isset($data['ancestor_id'])) {
-  //     $errors['ancestor_id'] = "Please fill in a ancestor id";
-  //   }
-  //   return $errors;
-  // }
+ 
 }

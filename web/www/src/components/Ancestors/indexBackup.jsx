954cf000ee33cd99b238410a0c500809d9537c42
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-06-20 14:13:12 +0200

    scroll icon hidden on scroll

--- web/www/src/components/Ancestors/indexBackup.jsx
+++ web/www/src/components/Ancestors/indexBackup.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useLayoutEffect, useRef, useMemo, forwardRe
 import { Canvas, createPortal } from 'react-three-fiber';
 import Controls from '../Controls';
 import Effects from './effects.jsx';
-import Lines from './lines.jsx';
 import Ancestor from '../Ancestor/index.jsx';
 import Sidebar from '../Sidebar/index.jsx';
 
@@ -13,28 +12,29 @@ import { useStore } from "../../hooks/useStore";
 import styles from './Ancestors.module.css';
 import { observe } from 'mobx';
 import * as THREE from 'three';
-import { useThree } from 'react-three-fiber'
 
 const Ancestors = () => {
+
+  const iconscroll = useRef();
+  // const canvasRef = useRef();
+
   const { ancestorStore } = useStore();
   const [preview, setPreview] = useState(false);
   const [ancestor, setAncestor] = useState(null);
   const [canvas, setCanvas] = useState(false);
   let ancestors = ancestorStore.ancestors;
 
-  const scrolliconRef = useRef();
-
   /* eerste keer site inladen */
   const stopObserve = observe(ancestorStore, (change) => {
     if (change.name === 'loadAllComplete') {
-      setCanvas(true);
+      setCanvas(<CanvasView />);
     }
   });
 
   /* wordt ook opgeroepen indien je komt vanuit ander component */
   useLayoutEffect(() => {
     if (ancestorStore.loadAllComplete && !canvas) {
-      setCanvas(true);
+      setCanvas(<CanvasView />);
     }
   }, [canvas, ancestors]);
 
@@ -45,16 +45,17 @@ const Ancestors = () => {
     setAncestor(clickedAncestor);
   };
 
-  const canvasCreated = (gl) => {
-    gl.setClearColor('#1c1c1c');
-    gl.domElement.addEventListener('wheel', () => {
-      scrolliconRef.current.classList.add(styles.iconscrollHidden);
-      console.log(scrolliconRef.current);
-      // setScrollicon(false);
-    });
-  }
+  var curve = new THREE.CubicBezierCurve(
+    new THREE.Vector2(-10, 0),
+    new THREE.Vector2(-5, 15),
+    new THREE.Vector2(20, 15),
+    new THREE.Vector2(10, 0)
+  );
 
-  const CanvasView = () => {
+  /* divisions -- number of pieces to divide the curve into. Default is 5. */
+  var points = curve.getPoints(50);
+
+  const CanvasView = forwardRef((props, canvasRef) => {
     return (
       <Canvas
         camera={{
@@ -63,12 +64,19 @@ const Ancestors = () => {
           near: 0.1,
           far: 500,
         }}
-        onCreated={({ gl }) => canvasCreated(gl)}
+        onCreated={({ gl }) => gl.setClearColor('#1c1c1c')}
+        onWheel={(e) => console.log('wheel spins')}
+        onClick={(e) => console.log("click")}
       >
-        <Lines />
         <Controls />
         <ambientLight color="#ffffff" intensity={0.1} />
         <pointLight position={[10, 10, 10]} />
+{/* 
+        <line visible position={[1, 2, 3]}>
+          <bufferGeometry attach="geometry" setFromPoints={points} />
+          <lineBasicMaterial color="0xff0000" />
+        </line> */}
+
         {ancestors.map((ancestor) => (
           <group
             key={ancestor.id}
@@ -78,9 +86,46 @@ const Ancestors = () => {
             <Ancestor ancestor={ancestor} ancestorStore={ancestorStore} />
           </group>
         ))}
+        {/* <Effects /> */}
       </Canvas>
     );
-  };
+  });
+
+   useLayoutEffect(() => {
+     if (ancestorStore.loadAllComplete && !canvas) {
+        setCanvas(<CanvasView />);
+     }
+   }, [canvas, ancestors]);
+
+   
+   // DIT VIND HIJ
+   console.log(iconscroll); // als ik het log vind hij het wel
+   
+
+  // const handleMouseScroll = (e) => {
+  //   //this.that.iconscroll.style.opacity -= Math.abs(e.deltaY) / 1000;
+  //   iconscroll.style.opacity -= Math.abs(e.deltaY) / 1000;
+  // }
+
+  //  useEffect(() => {
+  //    window.addEventListener(`wheel`, handleMouseScroll);
+  //  }, []);
+
+   //this.that.iconscroll.style.opacity = 1;
+
+
+  // PROBEERSEL
+   const [offset, setOffset] = useState(0);
+   useEffect(() => {
+     window.onscroll = () => {
+       setOffset(iconscroll.style.opacity = 0);
+     };
+   }, []);
+  console.log(window.onscroll); 
+
+  window.addEventListener("scroll", function () {
+    console.log("scroll")
+  });
 
   return (
     <>
@@ -90,12 +135,18 @@ const Ancestors = () => {
         toggle={preview}
         setToggle={setPreview}
       />
-      <div
-        ref={scrolliconRef}
-        className={canvas ? styles.iconscroll : ''}
-      />
+
+      <div // _scroll
+        className={styles.iconscroll}
+        // ref={(iconscroll) => {
+        //   this.iconscroll = iconscroll;
+        // }}
+        ref={iconscroll}
+      >
+        {` `}
+      </div>
       <div className={styles.canvas__container}>
-        {canvas ? <CanvasView /> : <Loader />}
+        {canvas ? canvas : <Loader />}
       </div>
     </>
   );
---
+++ web/www/src/components/Ancestors/lines.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useObserver } from 'mobx-react-lite';
+
+const Lines = () => {
+  return useObserver(() => (
+    <>
+
+    </>
+  ));
+};
+
+export default Lines;
